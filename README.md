075c5213f50a142f35f22571936bf3f9722ffd39
devesh <developer.deveshkumar@gmail.com>
2025-07-19 19:11:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + TypeScript + Vite + Tailwind
+
+This template provides a minimal setup to get React working in Vite with TailwindCSS, HMR and some ESLint rules.
+NOTE: Don't Forget to update the package name in package.json
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className='bg-amber-300'>
+        <ul className='list-inside list-disc px-2.5'>
+          <li>change package name in package.json</li>
+          <li>edit this file at src/App.tsx</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
